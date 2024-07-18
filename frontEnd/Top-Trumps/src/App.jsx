2cb339324b34294a5a70fd2564bee278751b4a72
Gavin-Persad <gavinapersad@gmail.com>
2024-07-18 13:34:00 +0100

    testing linting

--- frontEnd/Top-Trumps/src/App.jsx
+++ frontEnd/Top-Trumps/src/App.jsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-blue-600 text-5xl font-bold underline">
-        Football Top Trumps
-      </h1>
-    </>
+        Football Top Trumps!!!</h1></>
   );
 }
 
