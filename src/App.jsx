287516738f832af6a1513e55be4914ee1605c2a4
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-07-19 15:34:18 +0100

    echo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-600 text-5xl font-bold underline">
+        Football Top Trumps
+      </h1>
+    </>
+  );
+}
+
+export default App;
