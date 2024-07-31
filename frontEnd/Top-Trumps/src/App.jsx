960eb03d95f45e253a0b05b81670c26841561f02
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-07-31 15:05:22 +0100

    test blob url added to app.jsx img

--- frontEnd/Top-Trumps/src/App.jsx
+++ frontEnd/Top-Trumps/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1 className="text-blue-600 text-5xl font-bold underline">
         Football Top Trumps
       </h1>
+      <img src={"blob:nodedata:d02ad0cf-eeb3-4a0f-b34a-bd7682e6b5b3"}></img>
     </>
   );
 }
